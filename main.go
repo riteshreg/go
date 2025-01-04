8690def9218821b9aef19501fab0a25b9bd3dccc
riteshreg <riteshregmi85@gmail.com>
2025-01-04 20:54:21 +0545

    Add function examples: Sum, Subtract, Swap, and Split; refactor HelloRandomNumber

--- main.go
+++ main.go
@@ -1,10 +1,26 @@
 package main
 
 import (
+	"fmt"
+	"golang/functions"
 	"golang/greetings"
 )
 
 func main() {
 	greetings.Hello("Ritesh Khadka")
 	greetings.HelloRandomNumber()
+
+	// function package <<
+	functions.Sum(1, 2)
+	var subtract int = functions.Subtract(3, 2)
+	fmt.Println(subtract)
+
+	a, b := functions.Swap("Ritesh", "Khadka")
+	fmt.Printf("a=%s and b=%s \n", a, b)
+
+	first, secod := functions.Split(10)
+	fmt.Printf("first=%d and second=%d", first, secod)
+
+	// function package >>
+
 }
