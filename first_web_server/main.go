c4bb16bd141617d35ed106e3641ccc32d20ab7b0
riteshreg <riteshregmi85@gmail.com>
2025-01-23 22:10:33 +0545

    Add HTTP handlers for root and hello endpoints in main.go

--- first_web_server/main.go
+++ first_web_server/main.go
@@ -2,8 +2,17 @@ package main
 
 import (
 	"fmt"
+	"net/http"
 )
 
 func main() {
-	fmt.Printf("sdfa")
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprint(w, "Hello, World!", r.URL.Path)
+	})
+
+	http.HandleFunc("/hello", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprint(w, "Hello, Hello!")
+	})
+
+	http.ListenAndServe(":8080", nil)
 }
