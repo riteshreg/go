42512ef3f4042eeced3aee1de6ad901a2c782f9a
riteshreg <riteshregmi85@gmail.com>
2025-02-16 21:58:06 +1000

    Add string and runes example and update main function

--- go_by_example/main.go
+++ go_by_example/main.go
@@ -1,9 +1,9 @@
 package main
 
 import (
-	x "v0/pointers"
+	x "v0/string-and-runes"
 )
 
 func main() {
-	x.Pointers()
+	x.StringAndRunes()
 }
---
+++ go_by_example/string-and-runes/main.go
@@ -0,0 +1,20 @@
+package stringandrunes
+
+import "fmt"
+
+func StringAndRunes() {
+
+	myString := "bpaynepal.com \n sdf"
+
+	fmt.Println(myString)
+
+	// var newString string = `my name is ritesh// ssdfa`
+
+	myString = "dsfa"
+
+	fmt.Println(myString)
+
+	mystr := "Welcome to GeeksforGeeks"
+	fmt.Println("String:", mystr)
+
+}
