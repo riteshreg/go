19f8197c5fe8fd6e961d1a12b68a1819df62ef4a
riteshreg <riteshregmi85@gmail.com>
2025-03-05 09:14:53 +0545

    Implement Luhn algorithm for credit card validation and update main function to demonstrate usage

--- credit-card-validator/main.go
+++ credit-card-validator/main.go
@@ -1,7 +1,36 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"strconv"
+)
+
+func luhn(card_number string) bool {
+
+	var revert bool = false
+	var sum int = 0
+
+	for i := len(card_number) - 1; i >= 0; i-- {
+		num, err := strconv.Atoi(string(card_number[i]))
+		if err != nil {
+			return false
+		}
+
+		if revert {
+			num = num * 2
+			if num > 9 {
+				num = num - 9
+			}
+		}
+
+		sum += num
+		revert = !revert
+	}
+
+	return sum%10 == 0
+
+}
 
 func main() {
-	fmt.Println("credit card validator")
+	fmt.Println(luhn("3742454555400126"))
 }
